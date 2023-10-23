f29bcd951959be00d5a5a077dedbdd9fcab5ecbd
Kemuel Batista <kemuellima20@gmail.com>
2023-10-23 20:59:10 -0300

    feat: pets repository, use cases and tests

---
+++ src/repositories/in-memory/in-memory-pets-repository.ts
@@ -0,0 +1,26 @@
+import { Pet, Prisma } from '@prisma/client'
+import { PetsRepository } from '../pets-repository'
+import { randomUUID } from 'node:crypto'
+
+export class InMemoryPetsRepository implements PetsRepository {
+  public items: Pet[] = []
+
+  async create(data: Prisma.PetUncheckedCreateInput) {
+    const pet = {
+      id: randomUUID(),
+      name: data.name,
+      about: data.about,
+      age: data.age,
+      size: data.size,
+      energy_level: data.energy_level,
+      level_independence: data.level_independence,
+      ambience: data.ambience,
+      created_at: new Date(),
+      org_id: data.org_id,
+    }
+
+    this.items.push(pet)
+
+    return pet
+  }
+}
